bf977a308b8e934d07a2e5f1ff1e418c894ba14a
guicardoso0404 <guilhermecardoso0404@gmail.com>
2026-04-08 20:55:09 -0300

    fix: update API_URL to production endpoint

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/tarefas";
+const API_URL = "https://deploytaskflowbackend.onrender.com/tarefas";
 
 const form = document.getElementById("tarefaForm");
 const listaTarefas = document.getElementById("listaTarefas");
